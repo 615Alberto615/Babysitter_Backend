8b95e55dc8c2c33883ab9d6c67050a351722923c
615Alberto615 <a.encinas115@gmail.com>
2023-05-22 02:42:26 -0400

    principales arreglado

--- src/main/java/com/softbabysi/demo/Api/ChildApi.java
+++ src/main/java/com/softbabysi/demo/Api/ChildApi.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/babysitter")
+@RequestMapping("/api/v1/child")
 public class ChildApi {
     @Autowired
     private ChildBl childBl;
